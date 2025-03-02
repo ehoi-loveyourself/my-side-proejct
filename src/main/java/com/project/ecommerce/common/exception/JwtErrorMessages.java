bdb744c549c2060a1d0d2a90eccfd51d4ddfc7d0
belle <ehoi.loveyourself@gmail.com>
2025-03-02 14:33:33 +0900

    feat: Jwt 관련 예외 클래스 작성

---
+++ src/main/java/com/project/ecommerce/common/exception/JwtErrorMessages.java
@@ -0,0 +1,8 @@
+package com.project.ecommerce.common.exception;
+
+public class JwtErrorMessages {
+    public static final String EXPIRED_TOKEN = "JWT 토큰이 만료되었습니다.";
+    public static final String INVALID_TOKEN = "잘못된 JWT 토큰입니다.";
+    public static final String UNSUPPORTED_TOKEN = "지원되지 않는 JWT 토큰입니다";
+    public static final String WRONG_TOKEN = "JWT 토큰이 잘못되었습니다.";
+}
