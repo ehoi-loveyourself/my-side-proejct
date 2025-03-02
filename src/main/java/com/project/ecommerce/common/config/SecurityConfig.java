ad8d8014038f5e39a0efe35516579a53b73949ed
belle <ehoi.loveyourself@gmail.com>
2025-03-02 15:16:16 +0900

    feat: SecurityConfig 작성
    - JwtAuthenticationFilter 도 적용

---
+++ src/main/java/com/project/ecommerce/common/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.project.ecommerce.common.config;
+
+import com.project.ecommerce.common.utils.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // JWT 기반 인증을 사용할 때 보통 CSRF 보호가 필요 없어서 비활성화하는 게 일반적임.
+                .cors(cors -> {
+                })
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 인증 방식에서는 서버가 클라이언트의 세션을 저장하지 않으므로 이 설정이 필수적임.
+                )
+                .authorizeHttpRequests(auth -> auth
+                        // 회원가입, 로그인, H2 콘솔은 인증 없이 접근 가능
+                        .requestMatchers("/api/v1/users/register", "/api/v1/users/login").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers
+                        // H2 Console을 iFrame 내에서 정상적으로 띄우기 위해 필요함
+                        .frameOptions(frame -> frame.sameOrigin())
+                )
+                // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 추가하여 JWT 인증을 수행
+                // 이 설정이 없으면 JWT 기반 인증이 동작하지 않음.
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
