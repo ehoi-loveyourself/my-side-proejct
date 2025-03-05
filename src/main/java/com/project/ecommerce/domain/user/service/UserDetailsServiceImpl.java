50dcbdcb8f22df4e13d4f8dfb4b7e4833acc851e
belle <ehoi.loveyourself@gmail.com>
2025-03-05 22:13:53 +0900

    feat: UserDetailsServiceImpl 구현

---
+++ src/main/java/com/project/ecommerce/domain/user/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.project.ecommerce.domain.user.service;
+
+import com.project.ecommerce.common.exception.UserErrorMessages;
+import com.project.ecommerce.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(UserErrorMessages.NOT_FOUND_USER));
+
+    }
+}
