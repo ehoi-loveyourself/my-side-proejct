3bc690780014337b6218476abe4b3be411d14662
belle <ehoi.loveyourself@gmail.com>
2025-03-16 00:13:06 +0900

    feat: Product, Category 엔티티 작성

---
+++ src/main/java/com/project/ecommerce/domain/product/entity/ProductCategory.java
@@ -0,0 +1,27 @@
+package com.project.ecommerce.domain.product.entity;
+
+import com.project.ecommerce.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "product_category")
+public class ProductCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_category_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
---
+++ src/main/java/com/project/ecommerce/domain/product/entity/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.project.ecommerce.domain.product.entity;
+
+public enum ProductStatus {
+    ACTIVE, INACTIVE, OUT_OR_STOCK
+}
