e0811cbda3bd2a4d74e63d4ee60ed43f9864a119
belle <ehoi.loveyourself@gmail.com>
2025-03-19 00:27:30 +0900

    feat: 에러 메세지 추가

--- src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
+++ src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
@@ -4,4 +4,5 @@ public class ProductErrorMessages {
     public static final String NOT_FOUND_PRODUCT = "해당 상품을 찾을 수 없습니다.";
     public static final String CANNOT_UPDATE_PRODUCT = "해당 상품을 수정할 수 없습니다.";
     public static final String INVALID_PRICE = "가격은 100원 이상이어야 합니다";
+    public static final String NO_AUTHORIZATION = "권한이 없습니다.";
 }
