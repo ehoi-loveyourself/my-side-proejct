64f7a5ccf234572e26d4ea3a48edb14aef623baf
taeyi <ehoi.loveyourself@gmail.com>
2025-03-18 15:31:16 +0900

    feat: 5. 관련 에러 메시지 작성

--- src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
+++ src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
@@ -2,4 +2,6 @@ package com.project.ecommerce.common.exception;
 
 public class ProductErrorMessages {
     public static final String NOT_FOUND_PRODUCT = "해당 상품을 찾을 수 없습니다.";
+    public static final String CANNOT_UPDATE_PRODUCT = "해당 상품을 수정할 수 없습니다.";
+    public static final String INVALID_PRICE = "가격은 100원 이상이어야 합니다";
 }
