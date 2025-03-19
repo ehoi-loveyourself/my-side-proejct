6e88e3c3093bed2ac093d5dc847d28c19db4361d
taeyi <ehoi.loveyourself@gmail.com>
2025-03-19 15:19:14 +0900

    feat: 5. 재고 수정 관련 에러 메세지 작성

--- src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
+++ src/main/java/com/project/ecommerce/common/exception/ProductErrorMessages.java
@@ -5,4 +5,6 @@ public class ProductErrorMessages {
     public static final String CANNOT_UPDATE_PRODUCT = "해당 상품을 수정할 수 없습니다.";
     public static final String INVALID_PRICE = "가격은 100원 이상이어야 합니다";
     public static final String NO_AUTHORIZATION = "권한이 없습니다.";
+    public static final String STOCK_MUST_MORE_THAN_ZERO = "재고 수량은 0 이상이어야 합니다.";
+    public static final String CANNOT_UPDATE_PRODUCT_WITH_SAME_STOCK = "기존 재고 수량과 같습니다.";
 }
