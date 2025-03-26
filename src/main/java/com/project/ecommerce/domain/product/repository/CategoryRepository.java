8f6da744cc974a4ad5325329f735d0384cc7733d
taeyi <ehoi.loveyourself@gmail.com>
2025-03-26 17:22:10 +0900

    feat: 카테고리 삭제 API 리포지토리 작성

--- src/main/java/com/project/ecommerce/domain/product/repository/CategoryRepository.java
+++ src/main/java/com/project/ecommerce/domain/product/repository/CategoryRepository.java
@@ -3,6 +3,10 @@ package com.project.ecommerce.domain.product.repository;
 import com.project.ecommerce.domain.product.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
+
+    List<Category> findByParentCategory_Id(Long categoryId);
 }
