12b39b09f128f3bacc88a6396d91989389bb615f
belle <ehoi.loveyourself@gmail.com>
2025-03-28 23:31:32 +0900

    feat: 구현할 서비스 선언

--- src/main/java/com/project/ecommerce/domain/cart/service/CartService.java
+++ src/main/java/com/project/ecommerce/domain/cart/service/CartService.java
@@ -7,8 +7,11 @@ public interface CartService {
     CartDto.CartResponse getCart(Long userId);
 
     // 장바구니에 아이템 담기
+    CartDto.CartResponse addItemToCart(Long userId, CartDto.AddItemRequest request);
 
     // 장바구니의 아이템 수량 변경
+    CartDto.CartResponse updateItemQuantity(Long userId, CartDto.UpdateItemQuantityRequest request);
 
     // 장바구니에서 아이템 삭제
+    CartDto.CartResponse deleteItemFromCart(Long userId, Long productId);
 }
