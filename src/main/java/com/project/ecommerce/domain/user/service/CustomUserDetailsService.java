8ee89dc126220d062dc9c4f510d48be979228b4f
belle <ehoi.loveyourself@gmail.com>
2025-04-17 23:48:42 +0900

    refactor: User 엔티티가 직접 UserDetails를 구현하도록 하지 않고, CustomUserDetails 을 작성하여 분리함에 따른 CustomUserDetailsService 수정 + 클래스명도 수정

--- src/main/java/com/project/ecommerce/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/com/project/ecommerce/domain/user/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.project.ecommerce.domain.user.service;
 
 import com.project.ecommerce.common.exception.UserErrorMessages;
+import com.project.ecommerce.common.security.CustomUserDetails;
+import com.project.ecommerce.domain.user.entity.User;
 import com.project.ecommerce.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,13 +12,15 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(UserErrorMessages.NOT_FOUND_USER));
+
+        return CustomUserDetails.fromUserEntity(user);
     }
 }
