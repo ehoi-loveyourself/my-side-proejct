d2690ad62f3611de385bf42f4abe7ddb00174f25
belle <ehoi.loveyourself@gmail.com>
2025-04-22 21:49:06 +0900

    feat: 주문 생성시 필요한 request dto 작성

---
+++ src/main/java/com/project/ecommerce/domain/order/dto/OrderRequest.java
@@ -0,0 +1,53 @@
+package com.project.ecommerce.domain.order.dto;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class OrderRequest {
+
+    @Getter
+    public static class CreateRequest {
+        // 주문 아이템들
+        private List<OrderItemDto> orderItems;
+        // 배송 옵션
+        private DeliveryOption deliveryOption;
+
+        @Getter
+        public static class OrderItemDto {
+            private Long productId;
+            private int quantity;;
+            private BigDecimal price; // 구매 당시의 가격
+        }
+
+        public enum DeliveryOption {
+            DEFAULT_ADDRESS,    // 기본 배송지 사용
+            SAVED_ADDRESS,      // 저장된 다른 주소 사용
+            NEW_ADDRESS         // 새로운 주소 사용
+        }
+
+        // 저장된 다른 주소를 사용한다면
+        private Long addressId;
+
+        // 새로운 주소로 배송할 경우
+        private DeliveryInfoDto newDeliveryInfo;
+
+        @Getter
+        public static class DeliveryInfoDto {
+            // 주소
+            private String streetAddress;
+            private String city;
+            // 우편번호
+            private String zipCode;
+            // 수령인
+            private String recipientName;
+            // 수령인 연락처
+            private String recipientPhone;
+            // 배송 메시지
+            private String deliveryMessage;
+            // 주소록 저장 여부
+            private boolean saveToAddressBook;
+        }
+    }
+}
