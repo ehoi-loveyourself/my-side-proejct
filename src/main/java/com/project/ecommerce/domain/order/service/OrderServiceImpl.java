fbe7e592665a9a375e1e5c5f8dbf887935ed6417
belle <ehoi.loveyourself@gmail.com>
2025-04-22 21:49:42 +0900

    feat: 주문 생성 로직 구현 중 1. 사용자 정보 가져오기, 2. 배송지 처리하기

---
+++ src/main/java/com/project/ecommerce/domain/order/service/OrderServiceImpl.java
@@ -0,0 +1,101 @@
+package com.project.ecommerce.domain.order.service;
+
+import com.project.ecommerce.common.exception.AddressErrorMessages;
+import com.project.ecommerce.common.exception.AddressException;
+import com.project.ecommerce.common.exception.UserErrorMessages;
+import com.project.ecommerce.common.exception.UserException;
+import com.project.ecommerce.common.security.UserContext;
+import com.project.ecommerce.domain.order.dto.OrderRequest;
+import com.project.ecommerce.domain.user.entity.Address;
+import com.project.ecommerce.domain.user.entity.User;
+import com.project.ecommerce.domain.user.repository.AddressRepository;
+import com.project.ecommerce.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class OrderServiceImpl implements OrderService {
+    private final UserRepository userRepository;
+
+    private final TransactionTemplate transactionTemplate;
+    private final UserContext userContext;
+    private final AddressRepository addressRepository;
+
+    @Override
+    @Transactional
+    public OrderResponse.CreateResponse createOrder(OrderRequest.CreateRequest request) {
+        log.info("주문 생성 시작");
+
+        return transactionTemplate.execute(status -> {
+           try {
+               // 현재 로그인한 사용자 정보 가져오기
+               Long currentUserId = userContext.getCurrentUserId();
+               User user = userRepository.findById(currentUserId)
+                       .orElseThrow(() -> {
+                           log.error("주문 생성 실패: 사용자를 찾을 수 없음, userId={}", currentUserId);
+                           return new UserException(UserErrorMessages.NOT_FOUND_USER, HttpStatus.NOT_FOUND);
+                       });
+
+               // 배송지 정보 처리
+               Address shippingAddress;
+               switch (request.getDeliveryOption()) {
+                   case DEFAULT_ADDRESS -> {
+                        // 사용자의 기본 배송지 조회
+                       shippingAddress = addressRepository.findByUserAndIsDefault(user, true)
+                               .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_DEFAULT_ADDRESS, HttpStatus.NOT_FOUND));
+                   }
+                   case SAVED_ADDRESS -> {
+                       // 사용자가 직접 지정한 배송지
+                       if (request.getAddressId() == null) {
+                           throw new AddressException(AddressErrorMessages.REQUIRED_ADDRESS, HttpStatus.BAD_REQUEST);
+                       }
+
+                       shippingAddress = addressRepository.findByIdAndUser(request.getAddressId(), user)
+                               .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_ADDRESS, HttpStatus.NOT_FOUND));
+                   }
+                   case NEW_ADDRESS -> {
+                        // 새로운 배송지로 지정
+                       if (request.getNewDeliveryInfo() == null) {
+                           throw new AddressException(AddressErrorMessages.INVALID_NEW_ADDRESS, HttpStatus.BAD_REQUEST);
+                       }
+
+                       shippingAddress = Address.builder()
+                               .streetAddress(request.getNewDeliveryInfo().getStreetAddress())
+                               .city(request.getNewDeliveryInfo().getCity())
+                               .zipCode(request.getNewDeliveryInfo().getZipCode())
+                               .isDefault(false)
+                               .recipientName(request.getNewDeliveryInfo().getRecipientName())
+                               .recipientPhone(request.getNewDeliveryInfo().getRecipientPhone())
+                               .user(user)
+                               .build();
+
+                       if (request.getNewDeliveryInfo().isSaveToAddressBook()) {
+                           addressRepository.save(shippingAddress);
+                       }
+                   }
+                   default -> {
+                       throw new AddressException(AddressErrorMessages.INVALID_DELIVERY_OPTION, HttpStatus.BAD_REQUEST);
+                   }
+               }
+
+               // 주문 아이템 검증 및 상품 가져오기
+
+               // 주문 번호 생성하기
+
+               // 주문 엔티티 생성하기
+           }
+        });
+        // 1. 존재하는 사용자인지 확인
+        // 2. 주문 엔티티 생성
+        // 3. 주문 아이템 생성 및 설정
+        // 4. 주문 저장
+        return null;
+    }
+}
