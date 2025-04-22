f7185bf90acd4e7e6312820c6aa37547fb2db97b
belle <ehoi.loveyourself@gmail.com>
2025-04-22 21:48:48 +0900

    feat: 주문서비스에 필요한 메서드 선언

---
+++ src/main/java/com/project/ecommerce/domain/order/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.project.ecommerce.domain.order.service;
+
+import com.project.ecommerce.domain.order.dto.OrderRequest;
+import com.project.ecommerce.domain.order.entity.OrderStatus;
+
+import java.util.List;
+
+public interface OrderService {
+    // 주문 생성
+    OrderResponse.CreateResponse createOrder(OrderRequest.CreateRequest request);
+
+    // 주문 목록 조회
+    List<OrderResponse> getOrderByUserId(Long userId);
+
+    // 주문 상세 조회
+    OrderDetailResponse getOrderByOrderId(Long orderId);
+
+    // 주문 상태 변경
+    OrderDetailResponse updateOrderStatus(Long orderId, OrderStatus status);
+
+    // 주문 결제 처리 todo: 추가 필요
+
+    // 주문 취소
+    OrderDetailResponse cancelOrder(Long orderId);
+}
