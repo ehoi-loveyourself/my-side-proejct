1bd73f23c09abdda3606334d87262450034c123c
taeyi <ehoi.loveyourself@gmail.com>
2025-04-23 17:52:58 +0900

    feat: 주문 생성 메서드 계속 구현 중

--- src/main/java/com/project/ecommerce/domain/order/service/OrderServiceImpl.java
+++ src/main/java/com/project/ecommerce/domain/order/service/OrderServiceImpl.java
@@ -1,21 +1,29 @@
 package com.project.ecommerce.domain.order.service;
 
-import com.project.ecommerce.common.exception.AddressErrorMessages;
-import com.project.ecommerce.common.exception.AddressException;
-import com.project.ecommerce.common.exception.UserErrorMessages;
-import com.project.ecommerce.common.exception.UserException;
+import com.project.ecommerce.common.exception.*;
 import com.project.ecommerce.common.security.UserContext;
 import com.project.ecommerce.domain.order.dto.OrderRequest;
+import com.project.ecommerce.domain.order.entity.Order;
+import com.project.ecommerce.domain.product.entity.Product;
+import com.project.ecommerce.domain.product.repository.ProductRepository;
 import com.project.ecommerce.domain.user.entity.Address;
 import com.project.ecommerce.domain.user.entity.User;
 import com.project.ecommerce.domain.user.repository.AddressRepository;
 import com.project.ecommerce.domain.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,6 +35,7 @@ public class OrderServiceImpl implements OrderService {
     private final TransactionTemplate transactionTemplate;
     private final UserContext userContext;
     private final AddressRepository addressRepository;
+    private final ProductRepository productRepository;
 
     @Override
     @Transactional
@@ -44,52 +53,27 @@ public class OrderServiceImpl implements OrderService {
                        });
 
                // 배송지 정보 처리
-               Address shippingAddress;
-               switch (request.getDeliveryOption()) {
-                   case DEFAULT_ADDRESS -> {
-                        // 사용자의 기본 배송지 조회
-                       shippingAddress = addressRepository.findByUserAndIsDefault(user, true)
-                               .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_DEFAULT_ADDRESS, HttpStatus.NOT_FOUND));
-                   }
-                   case SAVED_ADDRESS -> {
-                       // 사용자가 직접 지정한 배송지
-                       if (request.getAddressId() == null) {
-                           throw new AddressException(AddressErrorMessages.REQUIRED_ADDRESS, HttpStatus.BAD_REQUEST);
-                       }
+               Address shippingAddress = resolveShippingAddress(request, user);
 
-                       shippingAddress = addressRepository.findByIdAndUser(request.getAddressId(), user)
-                               .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_ADDRESS, HttpStatus.NOT_FOUND));
-                   }
-                   case NEW_ADDRESS -> {
-                        // 새로운 배송지로 지정
-                       if (request.getNewDeliveryInfo() == null) {
-                           throw new AddressException(AddressErrorMessages.INVALID_NEW_ADDRESS, HttpStatus.BAD_REQUEST);
-                       }
-
-                       shippingAddress = Address.builder()
-                               .streetAddress(request.getNewDeliveryInfo().getStreetAddress())
-                               .city(request.getNewDeliveryInfo().getCity())
-                               .zipCode(request.getNewDeliveryInfo().getZipCode())
-                               .isDefault(false)
-                               .recipientName(request.getNewDeliveryInfo().getRecipientName())
-                               .recipientPhone(request.getNewDeliveryInfo().getRecipientPhone())
-                               .user(user)
-                               .build();
-
-                       if (request.getNewDeliveryInfo().isSaveToAddressBook()) {
-                           addressRepository.save(shippingAddress);
-                       }
-                   }
-                   default -> {
-                       throw new AddressException(AddressErrorMessages.INVALID_DELIVERY_OPTION, HttpStatus.BAD_REQUEST);
-                   }
-               }
-
-               // 주문 아이템 검증 및 상품 가져오기
-
+               // 주문 아이템 검증 및 상품 가져오기: 해당 상품이 있는지, 재고가 있는지
+               List<OrderItemInfo> orderItemInfos = validateAndGetOrderItems(request.getOrderItems());
+               
                // 주문 번호 생성하기
+               String orderNumber = generateOrderNumber();
 
-               // 주문 엔티티 생성하기
+               BigDecimal totalAmount = orderItemInfos.stream()
+                       .map(itemInfo -> )
+
+               Order order = Order.builder()
+                       .orderNumber(orderNumber)
+//                       .orderItems()
+                       .totalAmount()
+                       .deliveryAddress(shippingAddress)
+                       .deliveryMessage(request.getDeliveryMessage())
+                       .user(user)
+                       .build();
+
+
            }
         });
         // 1. 존재하는 사용자인지 확인
@@ -97,5 +81,81 @@ public class OrderServiceImpl implements OrderService {
         // 3. 주문 아이템 생성 및 설정
         // 4. 주문 저장
         return null;
+    }
+
+    private String generateOrderNumber() {
+        LocalDate today = LocalDate.now();
+        String datePart = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String randomPart = String.format("%04d", ThreadLocalRandom.current().nextInt(10000));
+        return datePart + "-" + randomPart;
+    }
+
+    private Address resolveShippingAddress(OrderRequest.CreateRequest request, User user) {
+        switch (request.getDeliveryOption()) {
+            case DEFAULT_ADDRESS -> {
+                // 사용자의 기본 배송지 조회
+                return addressRepository.findByUserAndIsDefault(user, true)
+                        .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_DEFAULT_ADDRESS, HttpStatus.NOT_FOUND));
+            }
+            case SAVED_ADDRESS -> {
+                // 사용자가 직접 지정한 배송지
+                if (request.getAddressId() == null) {
+                    throw new AddressException(AddressErrorMessages.REQUIRED_ADDRESS, HttpStatus.BAD_REQUEST);
+                }
+
+                return addressRepository.findByIdAndUser(request.getAddressId(), user)
+                        .orElseThrow(() -> new AddressException(AddressErrorMessages.NOT_FOUND_ADDRESS, HttpStatus.NOT_FOUND));
+            }
+            case NEW_ADDRESS -> {
+                // 새로운 배송지로 지정
+                if (request.getNewDeliveryInfo() == null) {
+                    throw new AddressException(AddressErrorMessages.INVALID_NEW_ADDRESS, HttpStatus.BAD_REQUEST);
+                }
+
+                Address shippingAddress = Address.builder()
+                        .streetAddress(request.getNewDeliveryInfo().getStreetAddress())
+                        .city(request.getNewDeliveryInfo().getCity())
+                        .zipCode(request.getNewDeliveryInfo().getZipCode())
+                        .isDefault(false)
+                        .recipientName(request.getNewDeliveryInfo().getRecipientName())
+                        .recipientPhone(request.getNewDeliveryInfo().getRecipientPhone())
+                        .user(user)
+                        .build();
+
+                if (request.getNewDeliveryInfo().isSaveToAddressBook()) {
+                    addressRepository.save(shippingAddress);
+                }
+
+                return shippingAddress;
+            }
+            default -> throw new AddressException(AddressErrorMessages.INVALID_DELIVERY_OPTION, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private List<OrderItemInfo> validateAndGetOrderItems(List<OrderRequest.CreateRequest.OrderItemDto> orderItemDtos) {
+        return orderItemDtos.stream()
+                .map(itemDto -> {
+                    Product product = productRepository.findById(itemDto.getProductId())
+                            .orElseThrow(() -> new ProductException(ProductErrorMessages.NOT_FOUND_PRODUCT, HttpStatus.NOT_FOUND));
+
+                    if (product.getStock() < itemDto.getQuantity()) {
+                        throw new ProductException(String.format(ProductErrorMessages.INSUFFICIENT_STOCK,
+                                product.getName(),
+                                itemDto.getQuantity(),
+                                product.getStock())
+                                , HttpStatus.BAD_REQUEST);
+                    }
+
+                    return new OrderItemInfo(product, itemDto.getQuantity());
+                })
+                .toList();
+    }
+
+    // 주문 아이템 정보를 담는 내부 클래스
+    @Getter
+    @AllArgsConstructor
+    private static class OrderItemInfo {
+        private Product product;
+        private int quantity;
     }
 }
